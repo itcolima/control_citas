43fa8d179bf4128102062053a5de78ac14686328
Gionani <montana_malboro_montana@hotmail.com>
2015-10-13 23:58:55 -0500

    Update employee.rb

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,5 +1,5 @@
 class Employee < ActiveRecord::Base
 	 validates :name, :salary, :phone, presence: true
 	 has_many :appointments 
-	 has_many :clients, through => :appointments
+	 has_many :clients, :through => :appointments
 end
