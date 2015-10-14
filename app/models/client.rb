0f0804d91f789314f5797a2452b3d4079ad05d11
Gionani <montana_malboro_montana@hotmail.com>
2015-10-14 00:32:07 -0500

    Update client.rb
    asociación a productos y provedores

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,4 +2,6 @@ class Client < ActiveRecord::Base
 	 validates :name, :birthday, :phone, presence: true
 	 has_many :appointments
 	 has_many :employyes, :through => :appointments
+	 has_many :products
+	 has_many :providers, :through => :products
 end
